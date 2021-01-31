0a3877537c23a80323219239a1a724f9a7c65f98
MXKecetyCN <47166184+MXKecetyCN@users.noreply.github.com>
2021-01-31 17:42:58 +0800

    Create iboxpayCOOKIE.js

---
+++ iboxpayCOOKIE.js
@@ -0,0 +1,16 @@
+
+
+//独立COOKIE文件     ck在``里面填写，多账号换行
+let iboxpayheaderVal= ``
+let refreshtokenVal= ``
+
+
+let iboxpaycookie = {
+  iboxpayheaderVal: iboxpayheaderVal,  
+  refreshtokenVal: refreshtokenVal,  
+  
+}
+
+module.exports =  iboxpaycookie
+  
+
