8fd55d4d201b25e4fb655f4319ca0fb8c8d5228b
dockere <78626524+dockere@users.noreply.github.com>
2021-02-20 21:26:38 +0800

    Update git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -21,7 +21,7 @@ ContentNewTask=${ShellDir}/new_task
 ContentDropTask=${ShellDir}/drop_task
 SendCount=${ShellDir}/send_count
 isTermux=${ANDROID_RUNTIME_ROOT}${ANDROID_ROOT}
-#ScriptsURL=https://gitee.com/lxk0301/jd_scripts
+ScriptsURL=https://gitee.com/lxk0301/jd_scripts
 ShellURL=https://github.com/dockere/jd-base
 
 ## 更新crontab，gitee服务器同一时间限制5个链接，因此每个人更新代码必须错开时间，每次执行git_pull随机生成。
@@ -53,20 +53,20 @@ function Git_PullShell {
 }
 
 ## 克隆scripts
-#function Git_CloneScripts {
-#  git clone -b master ${ScriptsURL} ${ScriptsDir}
-#  ExitStatusScripts=$?
-#  echo
-#}
+function Git_CloneScripts {
+  git clone -b master ${ScriptsURL} ${ScriptsDir}
+  ExitStatusScripts=$?
+  echo
+}
 
 ## 更新scripts
-#function Git_PullScripts {
-#  cd ${ScriptsDir}
-#  git fetch --all
-#  ExitStatusScripts=$?
-#  git reset --hard origin/master
-#  echo
-#}
+function Git_PullScripts {
+  cd ${ScriptsDir}
+  git fetch --all
+  ExitStatusScripts=$?
+  git reset --hard origin/master
+  echo
+}
 
 ## 用户数量UserSum
 function Count_UserSum {
@@ -322,11 +322,11 @@ fi
 ## 更新crontab
 [[ $(date "+%-H") -le 2 ]] && Update_Cron
 ## 克隆或更新js脚本
-#if [ ${ExitStatusShell} -eq 0 ]; then
-#  echo -e "--------------------------------------------------------------\n"
-#  [ -f ${ScriptsDir}/package.json ] && PackageListOld=$(cat ${ScriptsDir}/package.json)
-#  [ -d ${ScriptsDir}/.git ] && Git_PullScripts || Git_CloneScripts
-#fi
+if [ ${ExitStatusShell} -eq 0 ]; then
+  echo -e "--------------------------------------------------------------\n"
+  [ -f ${ScriptsDir}/package.json ] && PackageListOld=$(cat ${ScriptsDir}/package.json)
+  [ -d ${ScriptsDir}/.git ] && Git_PullScripts || Git_CloneScripts
+fi
 
 ## 执行各函数
 if [[ ${ExitStatusScripts} -eq 0 ]]
