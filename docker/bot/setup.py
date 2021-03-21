f2c7ea4ef1fcae025eed1208f92bebba0ea3a5ce
iouAkira <e.akimoto.akira@gmail.com>
2021-03-21 12:09:56 +0800

    update

--- docker/bot/setup.py
+++ docker/bot/setup.py
@@ -2,12 +2,12 @@
 # @Author    : iouAkira(lof)
 # @mail      : e.akimoto.akira@gmail.com
 # @CreateTime: 2020-11-02
-# @UpdateTime: 2020-11-03
+# @UpdateTime: 2021-03-21
 
 from setuptools import setup
 
 setup(
     name='jd-scripts-bot',
-    version='0.1',
+    version='0.2',
     scripts=['jd_bot', ],
 )
